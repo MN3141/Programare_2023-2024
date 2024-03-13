19c6e1bcec59998a2fc9e5ed783d6fd488862463
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-03-13 23:57:32 +0200

    Laborator programare web

---
+++ web/test1/test.js
@@ -0,0 +1,6 @@
+function conc(param0,param1)
+{   console.log(param0+param1);
+    return param0+param1;
+}
+conc("Da","NU");
+console.log(navigator.appName);//orice navigator(browser) modern va returna "Netscape"
