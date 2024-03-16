73989f93e0a8e4187e0e595cb91a2c3d07cebc30
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-03-16 18:19:38 +0200

    Tema OpenGL

--- familia_c/c/grafic/tema1/main.c
+++ familia_c/c/grafic/tema1/main.c
@@ -4,27 +4,28 @@
 const float pi = 3.14159;
 static GLfloat x = 0;
 static GLfloat y = 0;
-float length = 400;
+float length = 900;
 GLfloat angulus = 0;
 GLUquadric* obj = NULL;
+GLUquadric* obj2 = NULL;
 void CALLBACK MutaStanga(void)
 {
-    x = x - 600;
+    x = x - 10;
     printf("\n %s %f", "X:", x);
 }
 void CALLBACK MutaDreapta(void)
 {
-    x = x + 600;
+    x = x + 10;
     printf("\n %s %f", "X:", x);
 }
 void CALLBACK SUS(void)
 {
-    y = y + 600;
+    y = y + 10;
     printf("\n %s %f", "Y:", y);
 }
 void CALLBACK JOS(void)
 {
-    y = y - 600;
+    y = y - 10;
     printf("\n %s %f", "Y:", y);
 }
 void CALLBACK leftClick(void) {
@@ -53,6 +54,7 @@ void frame()
 }
 void axes()
 {
+    //pt depanare si orientare
     glColor3ub(255, 0, 0);
     glBegin(GL_LINES);
     glVertex2f(-length/2,0);
@@ -62,15 +64,15 @@ void axes()
        glEnd();
 }
 void cylinder()
-{
+{ 
     glPushMatrix();
     glColor3ub(0, 247, 255);
     float temp = -length / 2;
+    float radius = length / 12-20 ; //2*radius<=length/6
     ///TRANSLATIE,apoi ROTATIE!
     glTranslatef((2*temp+length/6)/2, length-2*length/6,0);
     glRotatef(-240, 1, 0, 0);
-    glRotatef(60, 0, 1, 1);
-    gluCylinder(obj, 40, 40,50,20,1);
+    gluCylinder(obj, radius, radius, length/6,20,1);
     glPopMatrix();
 }
 void prism6()
@@ -78,10 +80,196 @@ void prism6()
     glPushMatrix();
     glColor3ub(0, 26, 255);
     float temp = -length/2+length/6;
+    float radius = length / 12 - 20;
     glTranslatef((2 * temp + length / 6) / 2, length - 2 * length / 6, 0);
-    glRotatef(-120, 1, 0, 0);
-    glRotatef(90, 0, 1, 1);
-    gluCylinder(obj, length/20, length/20, length/10+20, 6, 1);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    gluCylinder(obj, radius,radius,length/6, 6, 1);
+    glPopMatrix();
+}
+void prism5()
+{
+    glPushMatrix();
+    glColor3ub(51, 255, 0);
+    float temp = -length / 2 + 2*length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, length - 2 * length / 6, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    gluCylinder(obj, radius, radius, length / 6, 5, 1);
+    glPopMatrix();
+}
+void prism3()
+{
+    glPushMatrix();
+    glColor3ub(245, 8, 221);
+    float temp = -length / 2 + 3 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, length - 2 * length / 6, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    gluCylinder(obj, radius, radius, length / 6, 3, 1);
+    glPopMatrix();
+}
+void cone()
+{
+    glPushMatrix();
+    glColor3ub(154, 27, 27);
+    float temp = -length / 2 + 4 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, length - 2 * length / 6, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    gluCylinder(obj, radius, 0, length / 6, 20, 1);
+    glPopMatrix();
+
+}
+void coneTrunk()
+{
+    glPushMatrix();
+    glColor3ub(91, 189, 35);
+    float temp = -length / 2 + 5 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, length - 2 * length / 6, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    gluCylinder(obj, radius, radius/2, length / 6, 20, 1);
+    glPopMatrix();
+}
+void squarePyramid()
+{
+    glPushMatrix();
+    glColor3ub(154, 27, 27);
+    float temp = -length / 2;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, 0, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(14.13, 0, 0, 1);
+    gluCylinder(obj, radius, 0, length / 6, 4, 1);
+    glPopMatrix();
+}
+void trigPyramid()
+{
+    glPushMatrix();
+    glColor3ub(0, 25, 51);
+    float temp = -length / 2+length/6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, 0, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    gluCylinder(obj, radius, 0, length / 6, 3, 1);
+    glPopMatrix();
+}
+void hexPyramid() {
+    glPushMatrix();
+    glColor3ub(0, 102, 0);
+    float temp = -length / 2 + 2*length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2 +x, 0+y, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(7.10, 0, 0, 1);
+    gluCylinder(obj, radius, 0, length / 6, 6, 1);
+    glPopMatrix();
+}
+void pentaPyramid() {
+    glPushMatrix();
+    glColor3ub(0, 102, 102);
+    float temp = -length / 2 + 3 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, 0, 0);
+    glRotatef(-230, 1, 0, 0);
+    glRotatef(20, 0, 0, 1);
+    gluCylinder(obj, radius, 0, length / 6, 5, 1);
+    glPopMatrix();
+}
+void diskCircle()
+{
+    glPushMatrix();
+    glColor3ub(153, 76, 102);
+    float temp = -length / 2 + 4 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, 0, 0);
+    glRotatef(angulus, 0, 0, 28.274296);
+    gluDisk(obj, 0, radius, 20, 1);
+    glPopMatrix();
+}
+void diskHexagon() {
+    glPushMatrix();
+    glColor3ub(0, 0, 255);
+    float temp = -length / 2 + 5 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, 0, 0);
+    glRotatef(28.274296, 0, 0, 1);
+    gluDisk(obj, 0, radius, 6, 1);
+    glPopMatrix();
+}
+void diskTriangle()
+{
+    glPushMatrix();
+    glColor3ub(0, 153, 0);
+    float temp = -length / 2 ;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, -2*length/3, 0);
+    glRotatef(28.274296, 0, 0, 1);
+    gluDisk(obj, 0, radius, 3, 1);
+    glPopMatrix();
+}
+void Sphere()
+{
+
+    glPushMatrix();
+    glColor3ub(22, 116, 119);
+    float temp = -length / 2+length/6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, -2 * length / 3, 0);
+    glRotatef(angulus, 1, 0, 0);
+    gluSphere(obj, radius, 15, 15);
+    glPopMatrix();
+}
+void Polyhedron4()
+{
+    glPushMatrix();
+    glColor3ub(200, 8, 119);
+    float temp = -length / 2 + 2*length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, -2 * length / 3, 0);
+    glRotatef(28.274296, 1, 0, 0);//28.274296
+    gluCylinder(obj, radius, radius, length / 6, 4, 4);
+    glPopMatrix();
+}
+void Polyhedron6()
+{
+    glPushMatrix();
+    glColor3ub(22, 7, 2);
+    float temp = -length / 2 + 3* length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, -2 * length / 3, 0);
+    glRotatef(10, 0,1, 0);
+    gluCylinder(obj, radius, radius, length / 6, 6, 6);
+    glPopMatrix();
+}
+void Polyhedron8()
+{
+    glPushMatrix();
+    glColor3ub(22, 0, 119);
+    float temp = -length / 2 + 4 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, -2 * length / 3, 0);
+    glRotatef(5, 0, 1, 0);
+    gluCylinder(obj, radius, radius, length / 6, 8, 8);
+    glPopMatrix();
+}
+void cakeSLice()
+{
+    glPushMatrix();
+    glColor3ub(43, 29, 20);
+    float temp = -length / 2 + 5 * length / 6;
+    float radius = length / 12 - 20;
+    glTranslatef((2 * temp + length / 6) / 2, -2 * length / 3, 0);
+    glRotatef(-230, 1, 0, 0);
+    gluCylinder(obj2, radius, radius, length / 6, 3, 1);
+    glColor3ub(255, 255, 0);
+    gluDisk(obj2, 0, radius, 3, 1);
     glPopMatrix();
 }
 void CALLBACK display(void)//creare scena
@@ -90,11 +278,27 @@ void CALLBACK display(void)//creare scena
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     frame();
-    //DE AJUSTAT DIMENSIUNEA CORPURILOR IN FUNCTIE DE length
     cylinder();
     prism6();
+    prism5();
+    prism3();
+    cone();
+    coneTrunk();
+    squarePyramid();
+    trigPyramid();
+    hexPyramid();
+    pentaPyramid();
+    diskCircle();
+    diskHexagon();
+    diskTriangle();
+    Sphere();
+    Polyhedron4();
+    Polyhedron6();
+    Polyhedron8();
+    cakeSLice();
     glFlush();
 }
+
 void CALLBACK myReshape(GLsizei w, GLsizei h)
 {
     if (!h) return;
@@ -112,6 +316,9 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
 void init()
 {
     obj = gluNewQuadric();
+    obj2 = gluNewQuadric();
+    gluQuadricDrawStyle(obj, GLU_SILHOUETTE);
+    gluQuadricDrawStyle(obj2, GLU_FILL);
     auxInitDisplayMode(AUX_SINGLE | AUX_RGB);//setare buffer
     auxInitPosition(00, 00, 1000, 1000);//setare coordonate fereastra
     auxInitWindow("Tema 1");
@@ -127,5 +334,7 @@ void init()
 int main(int argc, char** argv)
 {
     init();
+    gluDeleteQuadric(obj);
+    gluDeleteQuadric(obj2);
     return(0);
 }
