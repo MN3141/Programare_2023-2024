7667f3fce2ca7e46966ffebb1adda731ef523aee
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-06-21 09:04:46 +0300

    Exersare examen web

---
+++ web/tabela/script.js
@@ -0,0 +1,10 @@
+function btn0()
+{
+    alert("Ai apasat bine,bre!");
+}
+function btn1(){
+    alert("Prost ai apasat,pendejo!");
+}
+let fruits=["Apple","Orange","Plum"];
+fruits.shift();
+console.log(fruits);
