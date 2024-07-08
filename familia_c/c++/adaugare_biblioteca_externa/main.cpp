d5ba5f39a839bdc3ae2863d2e8681d23f737f90d
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-07-08 21:19:16 +0300

    Adaugare biblioteca C++ din alt director

---
+++ familia_c/c++/adaugare_biblioteca_externa/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<matrix.h>
+using std:: cout;
+using std:: endl;
+int main(int argc,char **argv)
+{
+    cout<<"Test";
+    Matrix m(2);
+    printf("\n Biblioteca adaugata");
+    return 0;
+}
