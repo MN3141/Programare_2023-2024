0c6ca29cfd22c4aeeee154deae887445ecb258f9
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-08-19 19:49:11 +0300

    Test winrar

---
+++ batch/tools_setup.bat
@@ -0,0 +1,16 @@
+::This is a demo script used for setting up some tools
+::and testing Github Actions
+
+::This script requires at least Windows 10
+set TOOLS_PATH=E:\tools
+set TEMP_DIR=temp
+mkdir %TOOLS_PATH%
+
+mkdir temp
+mkdir temp\notepad
+
+curl -L https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.8/npp.8.6.8.portable.x64.zip --ssl-revoke-best-effort --output %TEMP_DIR%\npp.zip
+winrar x -df temp\npp.zip %cd%\temp\notepad
+
+move %cd%\temp\notepad %TOOLS_PATH%
+rmdir /s /q temp
