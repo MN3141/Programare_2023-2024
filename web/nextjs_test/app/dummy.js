1f92cfdcfad7c1b4eb578c6cc14860e185527b13
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-08-21 21:41:32 +0300

    Test NextJS

---
+++ web/nextjs_test/app/dummy.js
@@ -0,0 +1,9 @@
+export default function DUMMY()
+{
+    return(
+        <div>
+            <p>This is a dummy paragraph</p>
+            <img src="https://mlops-guide.github.io/MLOps/CICDML/ci-cd.png" alt="CI/CD"/>
+        </div>
+    );
+}
