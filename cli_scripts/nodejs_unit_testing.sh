65d8df33fed44b4b86b337b70ee3aab9a918515c
Neag Mihai <117996028+MN3141@users.noreply.github.com>
2024-08-27 22:26:55 +0300

    Problema detectare director __test__

--- cli_scripts/nodejs_unit_testing.sh
+++ cli_scripts/nodejs_unit_testing.sh
@@ -5,18 +5,17 @@
 echo "Starting running unit tests for NodeJS"
 cd ../web
 
-NPM_PATH=$(which npm)
-echo $NPM_PATH
-
 dir_array=$(ls -d */)
 for dir in ${dir_array};
 do
     cd $dir 
-    echo $dir
-    
+    echo "==============================="
+    echo "Curent directory:$dir"
+    echo "==============================="
+
     npm ci #npm clean install
     # --install-links=false
-    if [ -d "$dir/__test__" ]; then
+    if [ -d "__test__" ]; then
         npm run test
     fi
 
